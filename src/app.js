9515e24f9419cace6a4a1891033cbe02403fa0b2
danrodmell <danrodriguezmelendez@gmail.com>
2025-03-31 20:02:01 -0600

    Add ESLint configuration and fix linting issues

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
 
 const app = express();
 
@@ -10,12 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Routes
+app.use('/api', routes);
+
 // Basic route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Craft API' });
 });
 
 // Error handling middleware
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
